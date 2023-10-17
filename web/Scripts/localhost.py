865b652e731b636d8e41e745df518107ee78c23b
Noman <nomander17@gmail.com>
2023-10-17 15:34:47 +0530

    Python script to open a localhost port to test JS.

---
+++ web/Scripts/localhost.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+import pprint
+
+app = Flask(__name__)
+
+@app.route('/MinorWebApp/<path:path>', methods=['POST'])
+def handle_post(path):
+    data = request.form.to_dict()
+    print(f"POST request received on /MinorWebApp/{path}")
+    pprint.pprint(data)
+    return f'POST request received on /MinorWebApp/{path}', 200
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=8080)
