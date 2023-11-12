2f9a735046dc650f2a90480e9014e76ea7bd006c
Noman <nomander17@gmail.com>
2023-11-12 18:19:09 +0530

    Error and Success output JS

---
+++ web/Scripts/showResponse.js
@@ -0,0 +1,36 @@
+/*
+To display an error add in <div class="error-alert"></div>
+To display an success add in <div class="success-alert"></div>
+Code for URL param handling:
+
+<script>
+    let params = (new URL(document.location)).searchParams;
+    let error = params.get("error");
+
+    if (error == "incorrect-password") {
+        showError("Incorrect email or password.");
+        params.delete('error');
+        window.history.replaceState({}, document.title, url.toString());
+        // history.replaceState(null, null, window.location.pathname);
+        // Above line also works, clears all URL params so I'm not using it.
+    }
+</script>
+*/
+function showError(message) {
+    var errorAlert = document.getElementById('error-alert');
+    errorAlert.innerHTML = message;
+    errorAlert.style.display = "block";
+    errorAlert.style.animation = 'none';
+    errorAlert.offsetHeight;
+    errorAlert.style.animation = null; 
+    window.location.hash = 'error-alert';
+}
+function showSuccess(message) {
+    var successAlert = document.getElementById('success-alert');
+    successAlert.innerHTML = message;
+    successAlert.style.display = "block";
+    successAlert.style.animation = 'none';
+    successAlert.offsetHeight;
+    successAlert.style.animation = null; 
+    window.location.hash = 'sucess-alert';
+}
